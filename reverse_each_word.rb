3a9a9b753d22c716aee9eccd854fcad6ebc1ba74
Jacques Potgieter <potgieter.g.jacques@gmail.com>
2020-05-31 17:00:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 
 def reverse_each_word(string)
-    new_array = [] 
-    array = string.split
-    new_array = array.collect {|i| i.reverse}
-    new_array.join(" ")  
+  new_array = [] 
+  array = string.split
+  new_array = array.collect {|i| i.reverse}
+  new_array.join(" ")  
   end 
 end
 
